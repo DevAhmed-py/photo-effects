624448fac123b02253ea1ada11e49a5ab25d0211
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-11-12 23:57:54 +0100

    commit

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
 ctx.fillText("Canvas text!!!", 100, 50)
+
+function uploadImage(e) {
+    ReadableStream.readDataURL(e.targetfile[0]);
+}
